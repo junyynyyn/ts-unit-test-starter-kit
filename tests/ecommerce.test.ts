f62700dc893d9ba005f74a77f30c90621c96a1ae
Jun <7540864+junyynyyn@users.noreply.github.com>
2024-08-21 13:52:35 +1000

    Testing pushign to git

--- tests/ecommerce.test.ts
+++ tests/ecommerce.test.ts
@@ -2,8 +2,10 @@ import { describe, it, expect, beforeEach } from "vitest";
 import {
   addProduct,
   addToCart,
+  applyShippingDiscount,
   calculateTotal,
   clearCart,
+  removeFromCart,
 } from "../src/ecommerce";
 
 let cart = {};
@@ -15,9 +17,57 @@ describe("E-commerce System", () => {
   });
 
   it("should calculate price of all products", () => {
+    // Arrange Act Assert
+    // Arrange
     addToCart("Soap", 2);
     addToCart("Shampoo", 2);
+    // Act
     const sum = calculateTotal();
+    // Assert
     expect(sum).toBe(600);
   });
+
+  it("Calculating multiples of 1 product", () => {
+    addToCart("Soap", 5);
+    addToCart("Soap", 2);
+
+    const sum = calculateTotal();
+    expect(sum).toBe(700);
+  });
+
+  it("Should add items to cart", () => {
+    let cart = {};
+    // Arrange Act Assert
+    // Act
+    cart = addToCart("Soap", 2);
+    // Assert
+    expect(cart["Soap"]).toBe(2);
+  });
+
+  it("Should be able to remove items from cart", () => {
+    let cart = {};
+    cart = addToCart("Soap", 2);
+    cart = removeFromCart("Soap", 2);
+    expect(cart["Soap"]).toBe(undefined);
+  })
+
+  it("Should run out of stock", () => {
+    addToCart("Soap", 5);
+    addToCart("Soap", 5);
+    // expect(addToCart("Soap", 1)).toThrowError('Cannot remove');
+    
+  });
+
+  it ("Apply shipping discount", () => {
+    let cart = {};
+    cart = addToCart("Soap", 5);
+    const sum = applyShippingDiscount(calculateTotal());
+    expect(sum).toBe(490);
+  })
+
+  it ("Does not apply discount if total is less than 500", () => {
+    addToCart("Soap", 4);
+    const sum = applyShippingDiscount(calculateTotal());
+    expect(sum).toBe(400);
+  })
 });
